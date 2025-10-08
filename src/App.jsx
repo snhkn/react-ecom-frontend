8d62aba467e5ddbbd23429dc2c7af2bb5e778eac
snhkn <senihaakin@gmail.com>
2025-10-08 15:17:28 +0300

    Set layout of product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { FaBeer } from 'react-icons/fa'
+import Products from './components/Products'
 
 function App() {
 
 
   return (
-    <div>
-      <h1 className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-        Welcome <FaBeer />
-      </h1>
-    </div>
+    <>
+      <Products />
+    </>
   );
 }
 
