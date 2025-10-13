12e4b364827a219ceb45387c2a5631deb564efa9
snhkn <senihaakin@gmail.com>
2025-10-13 13:01:54 +0300

    Manage loaders and error scenarios

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,36 +6,17 @@ import {useEffect} from "react";
 
 const Products = () => {
 
-    const isLoading = false;
-    const errorMessage = "";
+    const { isLoading, errorMessage } = useSelector(
+        (state) => state.errors
+    );
+
     const {products} = useSelector((state)=>state.products)
     const dispatch =useDispatch();
 
     useEffect(()=>{
         dispatch(fetchProducts());
     }, [dispatch]);
-    // const products = [
-    // {
-    //   productId: 652,
-    //   productName: "Iphone Xs max",
-    //   image: "https://placehold.co/600x400",
-    //   description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-    //   quantity: 10,
-    //   price: 1450.0,
-    //   discount: 10.0,
-    //   specialPrice: 1305.0,
-    // },
-    // {
-    //   productId: 654,
-    //   productName: "MacBook Air M2s",
-    //   image: "https://placehold.co/600x400",
-    //   description: "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
-    //   quantity: 0,
-    //   price: 2550.0,
-    //   discount: 20.0,
-    //   specialPrice: 2040.0,
-    // }
-    // ];
+
     return(
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[%90] 2xl:mx-auto">
             {isLoading ? (
@@ -49,7 +30,7 @@ const Products = () => {
                 </div>
             ):(
                 <div className="min-h-[700px]">
-                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:gird-cols-2 gap-y-6 gap-x-6">
+                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
                         {products &&
                         products.map((item, i) => <ProductCard key={i} {...item} />
 
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./ProductReducer";
+import { errorReducer } from "./ErrorReducer";
 
 export const store = configureStore({
     reducer:{
         products: productReducer,
+        errors: errorReducer,
     },
     preloadedState:{},
 });
