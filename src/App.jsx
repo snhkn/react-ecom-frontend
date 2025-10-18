f7e1d008cdfa0110ced7f76f4a292454e7f7a750
snhkn <senihaakin@gmail.com>
2025-10-18 12:17:31 +0300

    Build the Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
 import Navbar from './components/shared/Navbar'
 import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={ <Home />}/>
         <Route path='/products' element={ <Products />}/>
         <Route path='/about' element={ <About />}/>
+        <Route path='/contact' element={ <Contact />}/>
       </Routes>
     </Router>
   )
