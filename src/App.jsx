cdfacdf5518844008653dd7aa0011c2a8234deef
snhkn <senihaakin@gmail.com>
2025-10-19 11:17:04 +0300

    Build cart page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About'
 import Contact from './components/Contact'
 import React, { useState } from 'react'
 import { Toaster } from 'react-hot-toast'
+import Cart from './components/cart/Cart'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/products' element={ <Products />}/>
           <Route path='/about' element={ <About />}/>
           <Route path='/contact' element={ <Contact />}/>
+          <Route path='/cart' element={ <Cart />}/>
         </Routes>
       </Router>
       <Toaster position='bottom-center'/>
