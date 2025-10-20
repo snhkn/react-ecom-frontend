0ce141a31b17cdc00e96de09a958459ea6b07b8c
snhkn <senihaakin@gmail.com>
2025-10-20 19:26:11 +0300

    Allow users to remove items from cart

--- src/components/cart/ItemContent.jsx
+++ src/components/cart/ItemContent.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { HiOutlineTrash } from "react-icons/hi";
 import SetQuantity from "./SetQuantity";
 import { useDispatch } from "react-redux";
-import { decreaseCartQuantity, increaseCartQuantity } from "../../store/actions";
+import { decreaseCartQuantity, increaseCartQuantity, removeFromCart } from "../../store/actions";
 import toast from "react-hot-toast";
 
 
@@ -37,6 +37,10 @@ const ItemContent = ({
         }
     };
 
+    const removeItemFromCart = (cartItems) => {
+        dispatch(removeFromCart(cartItems, toast));
+    };
+
      return (
         <div className="grid md:grid-cols-5 grid-cols-4 md:text-md text-sm gap-4   items-center  border-[1px] border-slate-200  rounded-md  lg:px-4  py-4 p-2">
             <div className="md:col-span-2 justify-self-start flex  flex-col gap-2 ">
@@ -55,7 +59,15 @@ const ItemContent = ({
 
                 <div className="flex items-start gap-5 mt-3">
                     <button
-                        onClick={() => {}}
+                        onClick={() => removeItemFromCart({
+                            image,
+                            productName,
+                            description,
+                            specialPrice,
+                            price,
+                            productId,
+                            quantity,
+                        })}
                         className="flex items-center font-semibold space-x-2 px-4 py-1 text-xs border border-rose-600 text-rose-600 rounded-md hover:bg-red-50 transition-colors duration-200">
                         <HiOutlineTrash size={16} className="text-rose-600"/>
                         Remove
