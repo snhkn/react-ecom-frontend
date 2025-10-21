d6baef4f5a19be271d03db3bcf635716114e8256
snhkn <senihaakin@gmail.com>
2025-10-21 11:24:57 +0300

    Update navbar on authentication

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
     const { cart } = useSelector((state) => state.carts);
+    const { user } = useSelector((state) => state.auth);
 
     return (
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -75,6 +76,11 @@ const Navbar = () => {
                    </Link>
                 </li>
 
+                { (user && user.id) ? (
+                    <li className="font-[500] transition-all duration-150">
+                        <p>Welcome</p>
+                    </li>
+                ) :(
 
                 <li className="font-[500] transition-all duration-150">
                    <Link className="flex items-center space-x-2 px-4 py-[6px]
@@ -87,6 +93,7 @@ const Navbar = () => {
                         <span>Login</span>
                    </Link>
                 </li>
+            )}
             </ul>
 
             <button
