5648ea2f91105388d38b8e98fdac4cf504f5fde7
snhkn <senihaakin@gmail.com>
2025-10-21 11:16:06 +0300

    Implement authenticating users with backend

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -113,3 +113,23 @@ export const removeFromCart = (data, toast) => (dispatch, getState) => {
     localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
 
 };
+
+export const authenticateSignInUser =
+    (sendData, toast, reset, navigate, setLoader) => async (dispatch) =>{
+
+        try{
+            setLoader(true);
+            const {data} = await api.post("/auth/signin", sendData);
+            dispatch({type: "LOGIN_USER", payload:data});
+            localStorage.setItem("auth", JSON.stringify(data));
+            reset();
+            toast.success("Login Success");
+            navigate("/");
+        }catch(error){
+            console.log(error);
+            toast.error(error?.response?.data?.message || "Internal server error");
+        }finally{
+            setLoader(false);
+        }
+
+};
--- src/store/reducers/AuthReducer.js
+++ src/store/reducers/AuthReducer.js
@@ -4,6 +4,12 @@ const initialState = {
 }
 
 export const authReducer = (state=initialState, action) => {
-    return state;
+    switch (action.type) {
+        case "LOGIN_USER":
+            return { ...state, user: action.payload };
+
+        default:
+            return state;
+    }
 };
 
